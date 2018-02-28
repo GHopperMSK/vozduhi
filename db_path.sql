a1e0b5c7d5d917d78c0b1e7dc60618e64138e9c9
GHopper <np25071984@gmail.com>
2018-02-28 14:20:17 +0300

    db_path fix

--- db_path.sql
+++ db_path.sql
@@ -1,7 +1,12 @@
+DROP TABLE IF EXISTS oc_manufacturer_description;
+
 CREATE TABLE oc_manufacturer_description (
-    `manufacturer_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `manufacturer_id` INT NOT NULL,
     `language_id` INT NOT NULL,
     `description` TEXT,
+    FOREIGN KEY (manufacturer_id)
+        REFERENCES oc_manufacturer(id)
+        ON DELETE CASCADE,
     FOREIGN KEY (language_id)
         REFERENCES oc_language(id)
         ON DELETE CASCADE
