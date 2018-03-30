c9298c54742834a6e19fcecafbf4310840a818fd
GHopper <np25071984@gmail.com>
2018-03-30 13:13:12 +0300

    ModelToolImage::resize 4th param

---
+++ admin/config.php
@@ -0,0 +1,33 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test3.test/admin/');
+define('HTTP_CATALOG', 'http://test3.test/');
+// HTTPS
+define('HTTPS_SERVER', 'http://test3.test/admin/');
+define('HTTPS_CATALOG', 'http://test3.test/');
+// DIR
+define('DIR_APPLICATION', '/var/www/test3.test/upload/admin/');
+define('DIR_SYSTEM', '/var/www/test3.test/upload/system/');
+define('DIR_IMAGE', '/var/www/test3.test/upload/image/');
+define('DIR_STORAGE', '/var/www/test3.test/storage/');
+define('DIR_CATALOG', '/var/www/test3.test/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'vozduhi');
+define('DB_PASSWORD', 'vozduhi');
+define('DB_DATABASE', 'vozduhi');
+define('DB_PREFIX', 'oc_');
+define('DB_PORT', '3306');
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
